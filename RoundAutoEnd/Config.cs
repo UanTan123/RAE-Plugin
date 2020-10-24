94a15811ee675cc451cad38b3551fb7e7706309c
UanTan123 <62202857+UanTan123@users.noreply.github.com>
2020-10-24 22:05:15 +0900

    Add files via upload

---
+++ RoundAutoEnd/Config.cs
@@ -0,0 +1,19 @@
+﻿using Exiled.API.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoundAutoEnd
+{
+    public sealed class Config : IConfig
+    {
+        [Description("Are you on the plugin?")]
+        public bool IsEnabled { get; set; } = true;
+
+        [Description("How many seconds after the start of the round does the round end ? Defualt : 30minutes.")]
+        public float EndMessage { get; set; } = 1800f;
+    }
+}
---
+++ RoundAutoEnd/EventHandlers.cs
@@ -0,0 +1,42 @@
+﻿using Exiled.API.Features;
+using Exiled.Events.EventArgs;
+using MEC;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoundAutoEnd
+{
+    public class EventHandlers
+    {
+        public EventHandlers(Plugin plugin) => this.plugin = plugin;
+        private readonly Plugin plugin;
+
+        public void OnRoundStart()
+        {
+            plugin.Coroutines.Add(Timing.RunCoroutine(RAE()));
+        }
+
+        public void OnRoundEnd(RoundEndedEventArgs ev)
+        {
+            foreach (CoroutineHandle coroutine in plugin.Coroutines)
+                Timing.KillCoroutines(coroutine);
+        }
+
+        public void OnRoundRestarting()
+        {
+            foreach (CoroutineHandle coroutine in plugin.Coroutines)
+                Timing.KillCoroutines(coroutine);
+        }
+
+        private IEnumerator<float> RAE()
+        {
+            yield return Timing.WaitForSeconds(Plugin.Instance.Config.EndMessage);
+            RoundSummary.RoundLock = false;
+            RoundSummary.singleton.ForceEnd();
+            Log.Info($"요청하신 대로, {Plugin.Instance.Config.EndMessage}초 후 라운드가 종료되었습니다.");
+        }
+    }
+}
